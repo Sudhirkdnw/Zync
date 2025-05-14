c3042ae972a102b3229583a5b8bd3e7514df1d0f
Sudhir kumar <sudhirkdnw@gmail.com>
2025-05-14 19:52:41 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/retrytech/chatter/untitled/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.retrytech.chatter.untitled
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
+
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        flutterEngine.plugins.add(GoogleMobileAdsPlugin())
+    }
+}
